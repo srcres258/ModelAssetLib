ac2a06c6d0436a8eca4c577889a9bf4bfb0c4d78
src_resources <src.res.211@gmail.com>
2024-04-04 23:00:01 +0800

    Reorganise project structure
    Signed-off-by: src_resources <src.res.211@gmail.com>

--- core/rust/src/util.rs
+++ core/rust/src/util.rs
@@ -5,3 +5,7 @@ use jni::sys::jboolean;
 pub fn bool_to_jboolean(val: bool) -> jboolean {
     if val { 1 } else { 0 }
 }
+
+pub fn jboolean_to_bool(val: jboolean) -> bool {
+    if val == 0 { false } else { true }
+}
