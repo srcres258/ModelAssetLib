e36155d21116e634dc462b709871b2d7c95d691f
src_resources <src.res.211@gmail.com>
2024-04-16 17:17:03 +0800

    Clean up unused imports
    Signed-off-by: src_resources <src.res.211@gmail.com>

--- example/src/main/kotlin/top/srcres/mods/malexample/MALExample.kt
+++ example/src/main/kotlin/top/srcres/mods/malexample/MALExample.kt
@@ -2,15 +2,11 @@ package top.srcres.mods.malexample
 
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.entity.Entity
-import net.neoforged.fml.InterModComms
 import net.neoforged.fml.common.Mod
-import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 import top.srcres.mods.modelassetlib.ModelAssetLib
 import top.srcres.mods.modelassetlib.client.model.AssetedEntityModel
-import java.io.File
 
 @Mod(MALExample.MODID)
 object MALExample {
